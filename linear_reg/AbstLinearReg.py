db90bd9c0167252d3d424d42b2d72614f7812413
Karol Pietryka <karol.pietryka11@gmail.com>
2024-03-04 20:28:00 +0100

    [Add]Add csv save and calc time comparasion

--- linear_reg/AbstLinearReg.py
+++ linear_reg/AbstLinearReg.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import time
+import pandas as pd
 
 
 class AbsLinearReg(ABC):
@@ -19,17 +21,25 @@ class AbsLinearReg(ABC):
         w = 0.0
         b = 0.0
         self.n = len(training_data)
+        data = []
+
         mse = self.get_mse(training_data, target_data, w, b)
         print(f"Initial MSE: {mse}")
+        start_time = time.time()
         for i in range(self.iterations):
             gradients = self.get_gradients(training_data, target_data, w, b)
             w, b = self.gradient_descent(gradients, w, b)
             if i % 10 == 0:
                 mse = self.get_mse(training_data, target_data, w, b)
                 print(f"MSE for iteration {i}: {mse}. w: {w}, b: {b}")
+                data.append({'Iteration': i, 'MSE': mse, 'w': w, 'b': b, 'Total calc time': time.time() - start_time})
 
         mse = self.get_mse(training_data, target_data, w, b)
         print(f"Final w: {w}, and b: {b} with MSE: {mse}")
+
+        df = pd.DataFrame(data)
+        filename = f'{self.__class__.__name__}.csv'
+        df.to_csv(filename, index=False)
 
     def gradient_descent(self, gradients, w, b):
         w += -self.learning_rate * gradients[0]
