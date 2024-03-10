99dcc0abfb788e520b8731f1ffb5bc1d217fbf27
Karol Pietryka <karol.pietryka11@gmail.com>
2024-03-10 23:43:09 +0100

    [Add]Add iris project

---
+++ reg/Plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_decision_boundary(X, y, model):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    h = 0.01  # step size in the mesh
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', linewidth=1, marker='o', s=20)
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.show()
